a9711b431e7833c1fe2c35e280e77e875a039602
danieljdufour <daniel.j.dufour@gmail.com>
2017-03-07 04:42:34 +0000

    added better cell cleaning and debugging

--- table_extractor/__init__.py
+++ table_extractor/__init__.py
@@ -2,8 +2,10 @@ from broth import Broth
 from csv import reader as csv_reader
 from date_extractor import extract_date
 from docx import Document
+from io import BytesIO
 from openpyxl import load_workbook
 from os.path import isfile
+from re import match
 from requests import get
 from validators import url
 
@@ -15,33 +17,72 @@ def clean(value):
         date = extract_date(value)
         if date:
             return date
-        elif value.isdigit():
+        elif match("^\d+?\.\d+?$", value):
             return float(value)
+        elif value.isdigit():
+            return int(value)
+        else:
+            return value
 
-def extract_tables(data, debug=False):
+def extract_tables(data, debug=True):
 
-    if debug: print "type of data:", type(data)
-    if isinstance(data, file):
-        filename = data.name
-        if filename.endswith(".csv"):
-            return extract_tables_from_csv(data)
-        elif filename.endswith(".doc") or filename.endswith(".docx"):
-            return extract_tables_from_doc(data)
-        elif filename.endswith(".xls") or filename.endswith("xlsx"):
-            return extract_tables_from_excel_spreadsheet(data)
-        elif filename.endswith("pdf"):
-            return extract_tables_from_pdf(data)
-        elif filename.endswith("tsv"):
-            return extract_tables_from_tsv(data)
+    try:
 
-    elif isinstance(data, str) or isinstance(data, unicode):
-        if isfile(data):
-            with open(data) as f:
-                return extract_tables(f)
-        elif url(data):
-            return extract_tables_from_html(get(data).text)
-        elif data.count("<") > 5 and data.count(">") > 5:
-            return extract_tables_from_html(data)
+        if debug: print "starting extract_tables"
+        if debug: print "type of data:", type(data)
+
+        data_type = str(type(data))
+
+
+        unsupported_extensions = ("doc", "xls")
+
+        # we are not using isinstance here because the user might not have Django installed
+        if data_type in ("<type 'file'>", "<class 'django.core.files.uploadedfile.InMemoryUploadedFile'>"):
+            filename = data.name
+            extension = filename.split(".")[-1]
+            if filename.endswith(".csv"):
+                return extract_tables_from_csv(data)
+            elif filename.endswith(".docx"):
+                return extract_tables_from_doc(data)
+            elif extension in ("xlsx", "xlsm", "xltx", "xltm"):
+                return extract_tables_from_excel_spreadsheet(data)
+            elif filename.endswith("pdf"):
+                return extract_tables_from_pdf(data)
+            elif filename.endswith("tsv"):
+                return extract_tables_from_tsv(data)
+            elif extension in unsupported_extensions:
+                raise Exception("table-extractor does not the " + extension + " extension")
+
+        elif isinstance(data, str) or isinstance(data, unicode):
+            if isfile(data):
+                with open(data) as f:
+                    return extract_tables(f)
+            elif url(data):
+                extension = data.split(".")[-1]
+                #if extension in ("csv", "tsv", "xls", "xlsm", "xlsx"):
+                if extension == "csv":
+                    return extract_tables_from_csv(get(data).iter_lines())
+                elif extension == "docx":
+                    return extract_tables_from_doc(BytesIO(get(data).content))
+                elif extension == "tsv":
+                    return extract_tables_from_tsv(get(data).iter_lines())
+                elif extension in ("xlsx", "xlsm", "xltx", "xltm"):
+                    return extract_tables_from_excel_spreadsheet(BytesIO(get(data).content))
+                elif extension in unsupported_extensions:
+                    raise Exception("table-extractor does not the " + extension + " extension")
+                else:
+                    print "if not a known ending, assume it's html"
+                    print "should watch out for large files like videos"
+                    return extract_tables_from_html(get(data).text)
+
+            elif data.count("<") > 5 and data.count(">") > 5:
+                return extract_tables_from_html(data)
+
+        else:
+            print "[table-extractor] unhandled file type"
+
+    except Exception as e:
+        print "[table-extractor] caught exception in extract_tables:", e
 
 def extract_tables_from_excel_spreadsheet(excel_file):
     # can't do this all in one line with list comprehension because sometimes openpyxl and .xlsx adds a row at ends that's all Nones
@@ -57,15 +98,19 @@ def extract_tables_from_excel_spreadsheet(excel_file):
     return tables
      
 
-def extract_tables_from_csv(csvfile, delimiter=",", quotechar='"'):
+def extract_tables_from_csv(csvfile, delimiter=",", quotechar='"', debug=True):
     # assumes commas separated for now
     rows = []
     for row in csv_reader(csvfile, delimiter=delimiter, quotechar=quotechar):
+        if debug: print "row:", row
         values = [clean(cell) for cell in row]
+        if debug: print "values:", values
         set_of_values = set(values)
         if not(len(set_of_values) == 1 and set_of_values.pop() == None):
             rows.append(values)
-    return [rows]
+    tables = [rows]
+    if debug: print "[table-extractor] finishing extract_tables_from_csv with", tables
+    return tables
         
 
 def extract_tables_from_doc(doc_file):
